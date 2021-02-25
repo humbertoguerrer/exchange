96c6fd887acf73344f18bceba9c23a49add876ed
humbertoguerrer <humberto.guerrer@gmail.com>
2021-02-25 20:40:20 -0300

    adicionada a classe OperacaoService para tirar responsabilidade do OperacaoController

--- src/main/java/com/hgn/controller/OperacaoController.java
+++ src/main/java/com/hgn/controller/OperacaoController.java
@@ -2,26 +2,32 @@ package com.hgn.controller;
 
 import java.io.Serializable;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hgn.dto.OperacaoDTO;
-import com.hgn.entities.Operacao;
+import com.hgn.services.OperacaoService;
 
 @RestController
 public class OperacaoController implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Autowired
+	private OperacaoService operacaoService;
+	
 	@GetMapping(path = "/exchange/{amount}/{from}/{to}/{rate}")
-	public OperacaoDTO getOperacao(@PathVariable Integer amount, @PathVariable String from, @PathVariable String to,
+	public OperacaoDTO novaOPeracao(@PathVariable Integer amount, @PathVariable String from, @PathVariable String to,
 			@PathVariable Double rate) {
-		Operacao op1 = new Operacao(amount, from, to, rate);
-		op1.setConvertedAmount(amount, rate);
-		op1.setSymbol(to);
-		OperacaoDTO opDto = new OperacaoDTO(op1);
-
-		return opDto;
+		return operacaoService.novaOperacao(amount, from, to, rate);
+		
+		
+//		operacao = new Operacao(amount, from, to, rate);
+//		operacao.setConvertedAmount(amount, rate);
+//		operacao.setSymbol(to);
+//		OperacaoDTO opDto = new OperacaoDTO(operacao);
+//		return opDto;
 	}
 
 }
--- src/main/java/com/hgn/entities/Operacao.java
+++ src/main/java/com/hgn/entities/Operacao.java
@@ -2,6 +2,10 @@ package com.hgn.entities;
 
 import java.io.Serializable;
 
+import org.springframework.stereotype.Component;
+
+
+@Component
 public class Operacao implements Serializable {
 	private static final long serialVersionUID = 1L;
 
